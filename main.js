aa545ca11a376e0275d91e585bd8d073c88ed6e1
RyanKapoor620 <128708215+RyanKapoor620@users.noreply.github.com>
2023-07-14 15:12:42 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function preload(){
+}
+function setup(){
+    canvas=createCanvas(400,400);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(400,400);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelloaded);
+    poseNet.on("pose",gotposes);
+}
+function modelloaded(){
+    console.log("PoseNet is Loaded");
+}
+nosex=0;
+nosey=0;
+function gotposes(results){
+    if(results.length>0){
+        console.log(results);
+        nosex=results[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
+        console.log("Nose X = "+nosex+" Nose Y = "+nosey);
+    }
+}
+function draw(){
+    image(video,0,0,400,400);
+}
+function take_snapshot(){
+    save("Filtered_image.png");
+}
